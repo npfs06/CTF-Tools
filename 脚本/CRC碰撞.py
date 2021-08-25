87a7ad7777ccfaa0c680b9ee860d6941ea1e0359
npfs <79258733+npfs06@users.noreply.github.com>
2021-08-25 08:48:25 +0800

    Add files via upload

---
+++ 脚本/CRC碰撞.py
@@ -0,0 +1,28 @@
+#coding:utf-8
+import zipfile
+import string
+import binascii
+s=["红色", "橙色","黄色","绿色","青色"]
+def CrackCrc(crc):
+    for i in dic:
+        for j in dic:
+            for p in dic:
+                for q in dic:
+                    s = i + j + p + q
+                    if crc == (binascii.crc32(s) & 0xffffffff):
+                        f.write(s)
+                        return
+ 
+def CrackZip():
+    for I in s :
+        file = I+'.zip'
+        f = zipfile.ZipFile(file, 'r')
+        GetCrc = f.getinfo('data.txt')
+        crc = GetCrc.CRC
+        CrackCrc(crc)
+ 
+dic = string.ascii_letters + string.digits + '+/='
+ 
+f = open('out.txt', 'w')
+CrackZip()
+f.close()

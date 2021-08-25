f9661b6929fb9f8ad1786ec4a7a70c4d79e8b2c2
npfs <79258733+npfs06@users.noreply.github.com>
2021-08-25 08:54:36 +0800

    Add files via upload

---
+++ 脚本/取反.php
@@ -0,0 +1,8 @@
+<?php
+//在命令行中运行
+/*author yu22x*/
+fwrite(STDOUT,'[+]your function: ');
+$system=str_replace(array("\r\n", "\r", "\n"), "", fgets(STDIN));
+fwrite(STDOUT,'[+]your command: ');
+$command=str_replace(array("\r\n", "\r", "\n"), "", fgets(STDIN));
+echo '[*] (~'.urlencode(~$system).')(~'.urlencode(~$command).');';
